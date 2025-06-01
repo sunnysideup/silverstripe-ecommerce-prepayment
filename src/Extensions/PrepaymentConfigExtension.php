338b6a9b8744e030fd50e5c3eff5f225ce95562f
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-06-02 11:08:35 +1200

    FIX: better name for tab to match with names used elsewhere

--- src/Extensions/PrepaymentConfigExtension.php
+++ src/Extensions/PrepaymentConfigExtension.php
@@ -20,13 +20,11 @@ class PrepaymentConfigExtension extends DataExtension
     public function updateCMSFields(FieldList $fields)
     {
         $fields->addFieldsToTab(
-            'Root.Price',
+            'Root.Pricing',
             [
                 HTMLEditorField::create('PrepaymentMessageWithProduct', 'Generic Prepayment Message')
                     ->setDescription('This message will be shown on all products that have a prepayment percentage set and are not yet for sale proper.'),
             ]
         );
     }
-
-
 }
